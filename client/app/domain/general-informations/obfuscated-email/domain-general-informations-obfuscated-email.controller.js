d4d02cbd18b37613b04b8c7245827c70bcc956b4
Jérôme Martin <jerome.martin@corp.ovh.com>
2018-07-31 15:28:15 +0200

    feat(domain): integrate obfuscated emails page

--- client/app/domain/general-informations/obfuscated-email/domain-general-informations-obfuscated-email.controller.js
+++ client/app/domain/general-informations/obfuscated-email/domain-general-informations-obfuscated-email.controller.js
@@ -2,6 +2,19 @@ angular.module('App').controller(
   'DomainGeneralInformationsObfuscatedEmailCtrl',
   class DomainGeneralInformationsObfuscatedEmailCtrl {
     constructor() {
+      this.regenerate = {
+        owner: false,
+        tech: false,
+        billing: false,
+        admin: false,
+      };
+    }
+
+    canRegenerate() {
+      return !_.any(this.regenerate);
+    }
+
+    regenerate() {
 
     }
   },
