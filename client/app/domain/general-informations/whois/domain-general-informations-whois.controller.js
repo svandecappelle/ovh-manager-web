16664932b8338a530eb2f3b56aefbc6eec86c036
Jérôme Martin <jerome.martin@corp.ovh.com>
2018-08-02 14:32:23 +0200

    feat(domain): integrate whois subpage

--- client/app/domain/general-informations/whois/domain-general-informations-whois.controller.js
+++ client/app/domain/general-informations/whois/domain-general-informations-whois.controller.js
@@ -2,6 +2,15 @@ angular.module('App').controller(
   'DomainGeneralInformationsWhoisCtrl',
   class DomainGeneralInformationsWhoisCtrl {
     constructor() {
+      this.contacts = {
+        owner: { showEmail: false, showCoordinates: false },
+        admin: { showEmail: false, showCoordinates: false },
+        tech: { showEmail: false, showCoordinates: false },
+      };
+      this.contactTypes = _.keys(this.contacts);
+    }
+
+    validate() {
 
     }
   },
