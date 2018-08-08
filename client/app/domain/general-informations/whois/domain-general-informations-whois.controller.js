8a736a4d78d2129508b6bdf00de6576c52023552
Jérôme Martin <jerome.martin@corp.ovh.com>
2018-08-08 17:22:21 +0200

    feat(domain): handle field by field whois masking

--- client/app/domain/general-informations/whois/domain-general-informations-whois.controller.js
+++ client/app/domain/general-informations/whois/domain-general-informations-whois.controller.js
@@ -2,12 +2,21 @@ angular.module('App').controller(
   'DomainGeneralInformationsWhoisCtrl',
   class DomainGeneralInformationsWhoisCtrl {
     constructor() {
-      this.contacts = {
-        owner: { showEmail: false, showCoordinates: false },
-        admin: { showEmail: false, showCoordinates: false },
-        tech: { showEmail: false, showCoordinates: false },
+      const contactModel = {
+        showEmail: false, // should we show email in whois?
+        showCoordinates: false, // should we show other coordinates in whois?
+        fieldByField: false, // should we detail coordinates (priority over 'showCoordinates' value)
+        fields: { // list of coordinates to show/hide
+          name: false,
+          firstname: false,
+          organisationName: false,
+          address: false,
+          tel: false,
+        },
       };
-      this.contactTypes = _.keys(this.contacts);
+      this.contactTypes = ['owner', 'admin', 'tech'];
+      this.contacts = _.mapValues(_.mapKeys(this.contactTypes), () => _.cloneDeep(contactModel));
+      this.fields = _.keys(contactModel.fields);
     }
 
     validate() {
