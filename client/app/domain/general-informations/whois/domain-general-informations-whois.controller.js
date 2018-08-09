f97ed7ebca80f6ca17b13a82edf61bc3b9d0b5e6
Jérôme Martin <jerome.martin@corp.ovh.com>
2018-08-09 16:18:59 +0200

    feat(whois): get fields from the API

--- client/app/domain/general-informations/whois/domain-general-informations-whois.controller.js
+++ client/app/domain/general-informations/whois/domain-general-informations-whois.controller.js
@@ -1,26 +1,31 @@
 angular.module('App').controller(
   'DomainGeneralInformationsWhoisCtrl',
   class DomainGeneralInformationsWhoisCtrl {
-    constructor() {
-      const contactModel = {
-        showEmail: false, // should we show email in whois?
-        showCoordinates: false, // should we show other coordinates in whois?
-        fieldByField: false, // should we detail coordinates (priority over 'showCoordinates' value)
-        fields: { // list of coordinates to show/hide
-          name: false,
-          firstname: false,
-          organisationName: false,
-          address: false,
-          tel: false,
-        },
-      };
-      this.contactTypes = ['owner', 'admin', 'tech'];
-      this.contacts = _.mapValues(_.mapKeys(this.contactTypes), () => _.cloneDeep(contactModel));
-      this.fields = _.keys(contactModel.fields);
+    constructor(domainOperationService) {
+      this.DomainOperation = domainOperationService;
+
+      this.getContactFields().then((fields) => {
+        this.fields = fields;
+        const contactModel = {
+          showEmail: false, // should we show email in whois?
+          showCoordinates: false, // should we show other coordinates in whois?
+          fieldByField: false, // should we detail coordinates (priority over 'showCoordinates')
+          fields: _.mapValues(_.mapKeys(fields), () => false),
+        };
+        this.contactTypes = ['owner', 'admin', 'tech'];
+        this.contacts = _.mapValues(_.mapKeys(this.contactTypes), () => _.cloneDeep(contactModel));
+      });
     }
 
     validate() {
 
+    }
+
+    getContactFields() {
+      const except = ['id', 'email'];
+      return this.DomainOperation.getOperationModels()
+        .then(model => _.keys(model.models['contact.Contact'].properties))
+        .then(fields => _.difference(fields, except));
     }
   },
 );
